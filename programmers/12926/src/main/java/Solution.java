ab6d79c86c5c8ca4a1f2adb85365370cfbb96d83
stephano-tri <mtriumph23@gmail.com>
2024-01-10 17:52:00 +0900

    ADD : programmers 12926

--- programmers/12926/src/main/java/Solution.java
+++ programmers/12926/src/main/java/Solution.java
@@ -1,14 +1,45 @@
+import java.util.ArrayList;
+
 class Solution {
     public String solution(String s, int n) {
-        char[] lowerAlphabet = new char[26];
-        char[] upperAlphabet = new char[26];
+        String answer = "";
 
-        for(char a = 'a' ; a < 'z' ; a++){
-            lowerAlphabet[a - 'a'] = a;
-            upperAlphabet[a - 'a'] =
+        ArrayList<Character> lowerAlphabet = new ArrayList<Character>();
+        ArrayList<Character> upperAlphabet = new ArrayList<Character>();
+
+        for(char a = 'a' ; a <= 'z' ; a++){
+            lowerAlphabet.add(a);
+            upperAlphabet.add((char) (a + ('A' - 'a')));
         }
 
-        String answer = "";
+        for(char elem : s.toCharArray()){
+            if(elem != ' ') {
+                boolean isUpper = Character.isUpperCase(elem);
+                if(isUpper){
+                    answer = getString(n, answer, upperAlphabet, elem);
+                }
+                else {
+                    answer = getString(n, answer, lowerAlphabet, elem);
+                }
+            }
+            else {
+                answer += ' ';
+            }
+        }
+
+        return answer;
+    }
+
+    private String getString(int n, String answer, ArrayList<Character> lowerAlphabet, char elem) {
+        int srcIdx = lowerAlphabet.indexOf(elem);
+        int ceaserCnt = n;
+        if(srcIdx + n > 25){
+            ceaserCnt = srcIdx + n - 26;
+            answer += lowerAlphabet.get(ceaserCnt);
+        }
+        else {
+            answer += lowerAlphabet.get(srcIdx + ceaserCnt);
+        }
         return answer;
     }
 }
