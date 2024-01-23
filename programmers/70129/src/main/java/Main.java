37cca29e39c822b05cf276247bfe6bcf4330db6d
stephano-tri <mtriumph23@gmail.com>
2024-01-23 17:52:10 +0900

    FEATURE : 70129

---
+++ programmers/70129/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] sol = new Solution().solution("110010101001");
+    }
+}
--- programmers/70129/src/main/java/Solution.java
+++ programmers/70129/src/main/java/Solution.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+
 public class Solution {
+    ArrayList<Integer> zCnt = new ArrayList<Integer>();
+
     public int[] solution(String s) {
-        int[] answer = {};
-        return answer;
+        while(!s.equals("1")){
+            s = removeZero(s);
+            System.out.println(s);
+        }
+        // sum all zCnt's element using stream()
+        int zSum = zCnt.stream().mapToInt(Integer::intValue).sum();
+        int zLength = zCnt.size();
+
+        return new int[]{zLength,zSum};
+    }
+
+    public String removeZero(String s){
+        StringBuilder result = new StringBuilder();
+        int zeroCounter = 0;
+        for(char elem : s.toCharArray()){
+            if(elem != '0'){
+                result.append(elem);
+            }
+            else {
+                zeroCounter++;
+            }
+        }
+        zCnt.add(zeroCounter);
+        String targetLength = String.valueOf(result.toString().length());
+        //convert decimal to binary string
+        return Integer.toBinaryString(Integer.parseInt(targetLength));
     }
 }
