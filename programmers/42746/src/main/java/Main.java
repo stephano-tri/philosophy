2efae5abe8bbcfd4963c2f74c835b95f58976140
stephano-tri <mtriumph23@gmail.com>
2024-03-06 17:39:19 +0900

    FEATURE : 42746 added

--- programmers/42746/src/main/java/Main.java
+++ programmers/42746/src/main/java/Main.java
@@ -1,12 +1,59 @@
+import java.util.*;
+
 public class Main {
-    static class Solution {
+
+    // not worked way because it allows 100,000 elements..
+   /* static class Solution {
+        HashSet<String> permInts = new HashSet<String>();
+
         public String solution(int[] numbers) {
             String answer = "";
-            return answer;
+
+            boolean[] visited = new boolean[numbers.length];
+
+            perm(numbers , "" , 0 , numbers.length, visited);
+
+            ArrayList<String> ints = new ArrayList<>(permInts);
+
+            ints.sort(Collections.reverseOrder());
+
+            return ints.get(0);
+        }
+
+        public void perm(int[] numbers, String curr ,int cnt, int cond,boolean[] visited){
+            if(cnt == cond) {
+                permInts.add(curr);
+                return;
+            }
+
+            for(int i = 0 ; i < numbers.length ; i++) {
+                if(visited[i]) continue;
+
+                visited[i] = true;
+                String latestCurr = curr + numbers[i];
+                perm(numbers, latestCurr, cnt + 1, cond, visited);
+                visited[i] = false;
+            }
+        }
+    }*/
+
+    static class Solution {
+        public String solution(int[] numbers) {
+            String[] nums = new String[numbers.length];
+
+            for(int i = 0 ; i < numbers.length; i ++){
+                nums[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(nums, (t1, t2) -> (t2 + t1).compareTo(t1+ t2) );
+
+            if(nums[0].equals("0")) return "0";
+            return String.join("",nums);
         }
     }
 
-    public void static main(String[] args) {
-
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution(new int[] {6,10,2});
     }
 }
