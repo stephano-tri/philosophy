d8f920b546bb9632aba69719a152d56b8c0d92a4
stephano-tri <mtriumph23@gmail.com>
2024-03-06 14:12:23 +0900

    FEATURE : add 12915

---
+++ programmers/12915/src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    static class Solution {
+        public String[] solution(String[] strings, int n) {
+            String[] answer = {};
+
+            Arrays.sort(strings, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    if(o1.charAt(n) - '0' < o2.charAt(n) - '0'){
+                        return -1;
+                    }
+                    else if(o1.charAt(n) - '0' == o2.charAt(n) - '0'){
+                        return o1.compareTo(o2);
+                    }
+                    else {
+                        return 1;
+                    }
+                }
+            });
+
+            return strings;
+        }
+    }
+
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(new String[]{"sun", "bed", "car"}, 1)));
+    }
+}
